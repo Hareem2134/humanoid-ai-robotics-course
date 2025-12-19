76f8f8858231a7ea96954e43891de3c04ee3f944
Hareem Farooqi <007hareem@gmail.com>
2025-12-19 17:43:07 +0500

    robotics course

--- frontend/docusaurus-site/.docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("C:\\Users\\haree\\Downloads\\humanoid-ai-robotics-course\\frontend\\docusaurus-site\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("C:\\Users\\haree\\Downloads\\humanoid-ai-robotics-course\\frontend\\docusaurus-site\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\haree\\Downloads\\humanoid-ai-robotics-course\\frontend\\docusaurus-site\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\haree\\Downloads\\humanoid-ai-robotics-course\\frontend\\docusaurus-site\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\haree\\Downloads\\humanoid-ai-robotics-course\\frontend\\docusaurus-site\\src\\css\\custom.css"),
-];
